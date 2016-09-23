5cda3318300dc7fffef934172c12ec579074a746
ecocq <cocqelodie@hotmail.com>
2016-09-23 15:11:55 -0400

    config avec nom combox

--- InterfaceElo/configuration.h
+++ InterfaceElo/configuration.h
@@ -5,6 +5,10 @@ typedef struct {
 
     int nbIteration;
     bool stepbystep;
+    string bobLocation;
+    string jessLocation;
+    string elsaLocation;
+
 }configuration;
 
 #endif // CONFIGURATION_H
